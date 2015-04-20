2181f419c0a2341aa5953fb7689706d0f6c07c1c
Benoit Sautel <benoit.sautel@gmail.com>
2015-04-20 22:05:57 +0200

    Add Python and Groovy examples

--- src/main/java/fr/fierdecoder/staticscriptengine/Script.java
+++ src/main/java/fr/fierdecoder/staticscriptengine/Script.java
@@ -18,33 +18,49 @@ public class Script {
         nashorn.eval("function returnStringArray() {return ['a', 'b'];}");
         nashorn.eval("function returnIntegerArray() {return [1, 2, 3];}");
         Invocable nashornInvocator = (Invocable) nashorn;
-        Object stringArrayResult = nashornInvocator.invokeFunction("returnStringArray");
-        System.out.println(convert(stringArrayResult));
-        Object integerArrayResult = nashornInvocator.invokeFunction("returnIntegerArray");
-        System.out.println(convert(integerArrayResult));
+        Object nashornStringArrayResult = nashornInvocator.invokeFunction("returnStringArray");
+        System.out.println(convert(nashornStringArrayResult, String.class));
+        Object nashornIntegerArrayResult = nashornInvocator.invokeFunction("returnIntegerArray");
+        System.out.println(convert(nashornIntegerArrayResult, Integer.class));
+        ScriptEngine groovy = scriptEngineManager.getEngineByName("groovy");
+        groovy.eval("def returnStringArray() { return ['a', 'b']}");
+        groovy.eval("def returnIntegerArray() { return [1, 2, 3]}");
+        Invocable groovyInvocator = (Invocable) groovy;
+        Object groovyStringArrayResult = groovyInvocator.invokeFunction("returnStringArray");
+        System.out.println(convert(groovyStringArrayResult, String.class));
+        Object groovyIntegerArrayResult = groovyInvocator.invokeFunction("returnIntegerArray");
+        System.out.println(convert(groovyIntegerArrayResult, Integer.class));
+        ScriptEngine python = scriptEngineManager.getEngineByName("python");
+        python.eval("def returnStringArray():\n    return ['a', 'b']");
+        python.eval("def returnIntegerArray():\n    return [1, 2, 3]");
+        Invocable pythonInvocator = (Invocable) python;
+        Object pythonStringArrayResult = pythonInvocator.invokeFunction("returnStringArray");
+        System.out.println(convert(pythonStringArrayResult, String.class));
+        Object pythonIntegerArrayResult = pythonInvocator.invokeFunction("returnIntegerArray");
+        System.out.println(convert(pythonIntegerArrayResult, Integer.class));
     }
 
-    private static List<String> convert(Object result) {
+    private static <T> List<T> convert(Object result, Class<T> type) {
         if (result instanceof ScriptObjectMirror) {
             ScriptObjectMirror scriptObjectMirror = (ScriptObjectMirror) result;
             if (scriptObjectMirror.isArray()) {
                 Collection<Object> values = scriptObjectMirror.values();
                 return values.stream()
-                        .map(Script::objectToString)
+                        .map(value -> objectToType(value, type))
                         .collect(toList());
             }
         } else if (result instanceof List) {
-            List<Object> list = (List<Object>) result;
+            List<Object> list = (List) result;
             return list.stream()
-                    .map(Script::objectToString)
+                    .map(value -> objectToType(value, type))
                     .collect(toList());
         }
         throw new IllegalArgumentException("");
     }
 
-    private static String objectToString(Object value) {
-        if (value instanceof String) {
-            return (String) value;
+    private static <T> T objectToType(Object value, Class<T> type) {
+        if (type.isAssignableFrom(value.getClass())) {
+            return (T) value;
         }
         throw new IllegalArgumentException(value + " must be of type String");
     }
