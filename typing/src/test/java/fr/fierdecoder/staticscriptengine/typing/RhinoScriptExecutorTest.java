7bd02bd2e6f53d303b38fbfad733e38e074a56cf
Benoit Sautel <benoit.sautel@gmail.com>
2015-04-28 21:56:43 +0200

    Remove Rhino test since it is no longer supported in Java 8

--- typing/src/test/java/fr/fierdecoder/staticscriptengine/typing/RhinoScriptExecutorTest.java
+++
@@ -1,7 +0,0 @@
-package fr.fierdecoder.staticscriptengine.typing;
-
-public class RhinoScriptExecutorTest extends AbstractScriptExecutorTest {
-    public RhinoScriptExecutorTest() {
-        super("JavaScript", "script.js");
-    }
-}
